cc7ad839fc05b8b52504cd5036da97192ffaffcf
Deshmukh-Anurag-25 <deshmukh.anurag0327@gmail.com>
2025-10-05 20:37:18 +0530

    283. Move Zeroes

--- src/Array/MoveZeroes.java
+++ src/Array/MoveZeroes.java
@@ -1,20 +1,44 @@
 package Array;
 
-class MoveZeroes {
+/**
+ * This class provides a method to move all zeroes in an integer array
+ * to the end while preserving the order of non-zero elements.
+ */
+public class MoveZeroes {
 
-    void swap(int[] nums, int i, int j){
-        int temp = nums[i];
-        nums[i] = nums[j];
-        nums[j] = temp;
-    }
-
+    /**
+     * Moves all zeros in the given array to the end while keeping
+     * the relative order of non-zero elements.
+     *
+     * @param nums the array of integers to modify in place
+     */
     public void moveZeroes(int[] nums) {
         int j = 0;
-        for(int i = 0; i < nums.length; i++){
-            if(nums[i] != 0){
-                swap(nums, i, j);
-                j++;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                nums[j++] = nums[i];
             }
+        }
+
+        while (j < nums.length) {
+            nums[j++] = 0;
+        }
+    }
+
+    /**
+     * Main method to test the moveZeroes functionality.
+     *
+     * @param args command-line arguments
+     */
+    public static void main(String[] args) {
+        MoveZeroes solution = new MoveZeroes();
+        int[] nums = {0, 1, 0, 3, 12};
+
+        solution.moveZeroes(nums);
+
+        System.out.print("Result: ");
+        for (int num : nums) {
+            System.out.print(num + " ");
         }
     }
 }
